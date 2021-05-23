8cc9d45dc240e9f821b93bb89b89eb090186d4f5
petak5 <peterurgos12345@gmail.com>
2021-05-23 21:45:17 +0200

    Add code

--- MemoryNotFreeing/ViewController.swift
+++ MemoryNotFreeing/ViewController.swift
@@ -6,21 +6,65 @@
 //
 
 import Cocoa
+import AVFoundation
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func openFiles(_ sender: Any?) {
+        let dialog = NSOpenPanel();
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseFiles          = true;
+        dialog.canChooseDirectories    = true;
+        dialog.allowsMultipleSelection = true;
 
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+        var paths = [String]()
+        
+        if (dialog.runModal() != NSApplication.ModalResponse.OK) {
+            return
+        }
+        
+        // Collect all file paths from selected urls
+        let urls = dialog.urls
+        
+        for url in urls {
+            // Add all files from subdirectories (recursively)
+            if let enumerator = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+                for case let fileURL as URL in enumerator {
+                    do {
+                        let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
+                        if fileAttributes.isRegularFile! {
+                            paths.append(fileURL.path)
+                        }
+                    } catch {
+                        print(error, fileURL)
+                    }
+                }
+            }
+        }
+        
+        // Now we have all file paths
+        for path in paths {
+            let fileUrl = URL(fileURLWithPath: path)
+            let asset = AVAsset(url: fileUrl) as AVAsset
+            
+            // MARK: - This causes the issue
+//            if asset.metadata.count > 0 {
+//                // Do nothing
+//            }
+            
+            // MARK: - This too
+//            for metadataItem in asset.metadata {
+//                // Do nothing
+//            }
+            
+            // MARK: - This is completely fine, it runs longer than code without it but it doesn't consume so much memory as options above.
+            // MARK:   So the problem must be connected to 'metadata' attribure of 'asset'
+//            if asset.duration.seconds > 0 {
+//                // Do nothing
+//            }
         }
     }
-
 
 }
 
