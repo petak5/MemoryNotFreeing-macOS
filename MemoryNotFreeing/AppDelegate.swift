60ee8b680bc4fe55f43ac1131124ae585536136b
petak5 <peterurgos12345@gmail.com>
2021-05-23 21:15:47 +0200

    Initial Commit

---
+++ MemoryNotFreeing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MemoryNotFreeing
+//
+//  Created by Peter Urgoš on 23/05/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
